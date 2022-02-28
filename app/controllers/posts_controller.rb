f833a1969b703a117352fac8a28405fa279d48fd
Mateusz Buczek <mtsbck@gmail.com>
2022-02-28 20:19:53 +0100

    jebany delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      {id: 1, name: "POST 1", address: "http://warpas.blog"},
-      {id: 2, name: "POST 2", address: "nie"},
-      {id: 3, name: "POST 3", address: "nie, zamsz"},
-      {id: 4, name: "POST 4", address: "nie, zamsz"},
-      {id: 5, name: "POST 5", address: "nie, zamsz"},
-      {id: 6, name: "POST 6", address: "nie, zamsz"}
-    ]
+    # @posts = [
+    #   {id: 1, name: "POST 1", address: "http://warpas.blog"},
+    #   {id: 2, name: "POST 2", address: "nie"},
+    #   {id: 3, name: "POST 3", address: "nie, zamsz"},
+    #   {id: 4, name: "POST 4", address: "nie, zamsz"},
+    #   {id: 5, name: "POST 5", address: "nie, zamsz"},
+    #   {id: 6, name: "POST 6", address: "nie, zamsz"}
+    # ]
+    # Post.create(name: "POST 1")
+    # p params
+    @posts = Post.all
   end
+
   def show
+    @post = Post.find(params[:id])
+    @zmienna = destroy_post_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+
+  resources :posts
+
+  # get('/posts', {to: 'posts#index'})
+  # get '/posts/:id', to: 'posts#show'
+  # get '/posts/cokolwiek', to: 'posts#cosinnego'
 end
