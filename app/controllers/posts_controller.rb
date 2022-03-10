b9964be0609937fd1c443f26822cf6d0ef1aaa24
Mateusz Buczek <mtsbck@gmail.com>
2022-03-10 12:33:21 +0100

    10/03

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,8 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to(posts_path, status: :see_other)
   end
 
   private
